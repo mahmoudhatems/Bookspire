1f6d27a1450f6aaddd6eca105d2344620a1096a3
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2025-02-21 23:12:01 +0200

    Refactor HomeView to use HomeViewBody and add CustomAppBar; update splash view navigation

---
+++ lib/features/home/presentation/widgets/custom_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+            ),
+            ListTile(
+              title: Text('Item 2'),
+            ),
+          ],
+        ),
+      ),
+      body: Row(
+        children: [
+           DrawerButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+           ),
+      
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:bookspire/features/home/presentation/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+       Column(
+        children: [
+          const CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
