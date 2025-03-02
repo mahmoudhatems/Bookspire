556416c9d1abb9bca6a7e87d6aae14c5202f30c5
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2025-03-02 14:30:39 +0200

    Enhance BookDetailsViewBody layout; integrate BookRating and CustomFeaturedListViewItem for improved book presentation

--- lib/features/home/presentation/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/widgets/book_details_view_body.dart
@@ -1,13 +1,18 @@
 import 'package:bookspire/core/utils/colors.dart';
+import 'package:bookspire/core/utils/styles.dart';
+import 'package:bookspire/features/home/presentation/widgets/book_rating.dart';
+import 'package:bookspire/features/home/presentation/widgets/custom_featured_list_viewitem.dart';
+import 'package:bookspire/features/home/presentation/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -20,7 +25,6 @@ class BookDetailsViewBody extends StatelessWidget {
             ),
             onPressed: () {
               // Add to favorites change fill color to red
-                 
             },
           ),
         ],
@@ -35,15 +39,57 @@ class BookDetailsViewBody extends StatelessWidget {
           },
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Book Details',
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+                child:const CustomFeaturedListViewItem(),
+              ),
+            ],
+          ),
+
+          SizedBox(height: 20.h),
+          // Book Title
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Too Late",
+                style: Styles.styleSemiBoldText20darkBlueJua.copyWith(
+                  fontSize: 24.sp,
+                ),
+                maxLines: 2,
+              ),
+            ],
+          ),
+          SizedBox(height: 10.h),
+          // Author
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Alex Michaelides",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: Styles.styleNormalText14GrayRoboto.copyWith(
+                  fontSize: 16.sp,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10.h),
+          // Rating
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: 20.w),
+              const BookRating(),
+            ],
+          ),
+        ],
       ),
     );
   }
