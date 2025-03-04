cadbb1ab7552dfeeb35eb901b1f1ebc9e027948f
mahmoudhatems <113213094+mahmoudhatems@users.noreply.github.com>
2025-03-04 15:27:46 +0200

    Enhance error handling in failures.dart and home_repo_implementation.dart to support DioException types

--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -3,6 +3,7 @@ import 'package:bookspire/core/services/api_service.dart';
 import 'package:bookspire/features/home/data/models/book_model/book_model.dart';
 import 'package:bookspire/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImplementation implements HomeRepo {
   final ApiService apiService;
@@ -20,7 +21,10 @@ class HomeRepoImplementation implements HomeRepo {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
-    } on Exception catch (e) {
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
       return left(ServerFailure(e.toString()));
     }
   }
